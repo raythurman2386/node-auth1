586d39ee7e76ad7153e74a3805dec1797a0c5225
raythurman2386 <raymondthurman5@gmail.com>
2020-01-14 21:38:17 -0500

    user models initially complete

--- api/users/users.models.js
+++ api/users/users.models.js
@@ -1,16 +1,34 @@
-const bcrypt = require('bcryptjs');
-const db = require('../../data/db-config');
+import bcrypt from 'bcryptjs';
+import db from '../../data/db-config';
 
 // find
 const find = () => {
-  return db('')
+  return db('users').select('id', 'username');
 }
 
 // findBy
+const findBy = (filter) => {
+  return db('users')
+    .where(filter)
+    .select('id', 'username', 'password');
+}
 
 // add
+const add = async (user) => {
+  user.password = await bcrypt.hash(user.password, 13);
+
+  const [id] = await db('users')
+    .insert(user);
+
+  return findById(id);
+}
 
 // findById
+const findById = (id) => {
+  return db('users')
+    .where({ id })
+    .first('id', 'username');
+}
 
 module.exports = {
   find,
