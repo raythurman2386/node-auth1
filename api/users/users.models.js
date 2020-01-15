bbc7184c0c278778c3e6c4a9f1201ae1b78d37b8
raythurman2386 <raymondthurman5@gmail.com>
2020-01-14 21:29:12 -0500

    added files, need to finish db

---
+++ api/users/users.models.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcryptjs');
+const db = require('../../data/db-config');
+
+// find
+const find = () => {
+  return db('')
+}
+
+// findBy
+
+// add
+
+// findById
+
+module.exports = {
+  find,
+  findBy,
+  add,
+  findById
+}
