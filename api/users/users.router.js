5ac211c94e3fd4caee6ecb0520d41948565eeb7d
raythurman2386 <raymondthurman5@gmail.com>
2020-01-14 22:02:21 -0500

    login working correctly

--- api/users/users.router.js
+++ api/users/users.router.js
@@ -1,7 +1,7 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
 import restricted from '../auth/restricted';
 import usersModel from '../users/users.models';
-import dbConfig from '../../data/db-config';
 
 const router = express.Router();
 
@@ -19,7 +19,15 @@ router
   // Login
   .post('/login', async (req, res, next) => {
     try {
+      const { username, password } = req.body;
+      const user = await usersModel.findBy({ username }).first();
+      const checkPassword = await bcrypt.compare(password, user.password);
 
+      if (user && checkPassword) {
+        return res.status(200).json({ message: `Welcome ${username}!` })
+      } else {
+        return res.status(401).json({ message: 'Invalid Credentials' })
+      }
     } catch (error) {
       next(error)
     }
