492c8cb60da624b9aa885ccc49bd36ae07b98399
raythurman2386 <raymondthurman5@gmail.com>
2020-01-15 19:43:49 -0500

    altered restricted middleware

--- api/auth/restricted.js
+++ api/auth/restricted.js
@@ -1,6 +1,3 @@
-import bcrypt from 'bcryptjs';
-import usersModel from '../users/users.models';
-
 const restricted = () => {
   const authError = {
     message: 'Invalid Credentials'
@@ -8,22 +5,11 @@ const restricted = () => {
 
   return async (req, res, next) => {
     try {
-      const { username, password } = req.headers;
-      if (!username || !password) {
+      if (req.session && req.session.user) {
+        next();
+      } else {
         return res.status(401).json(authError);
       }
-
-      const user = await usersModel.findBy({ username }).first();
-      if (!user) {
-        return res.status(401).json(authError)
-      }
-
-      const checkPass = await bcrypt.compare(password, user.password)
-      if (!checkPass) {
-        return res.status(401).json(authError)
-      }
-
-      next();
     } catch (error) {
       next(error)
     }
